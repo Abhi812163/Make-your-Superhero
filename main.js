7e66b28e46da51c4ad73af5808ea6de12dde2744
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-03-29 17:30:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -139,3 +139,39 @@ function my_keydown(e){
         new_image('thor_right_hand.png');
         console.log("V");
     }
+    function up(){
+        if(player_y>=0){
+            player_y=player_y-block_image_height;
+            console.log("Block image height= "+block_image_height);
+            console.log("When up arrow is pressed x= "+player_x+" y= "+player_y);
+            canvas.remove(player_object);
+            player_update();
+        }
+    }
+    function down(){
+        if(player_y<=0){
+            player_y=player_y+block_image_height;
+            console.log("Block image height= "+block_image_height);
+            console.log("When up arrow is pressed x= "+player_x+" y= "+player_y);
+            canvas.remove(player_object);
+            player_update();
+        }
+    }
+    function left(){
+        if(player_x>=0){
+            player_x=player_x-block_image_width;
+            console.log("Block image width= "+block_image_width);
+            console.log("When up arrow is pressed x= "+player_x+" y= "+player_y);
+            canvas.remove(player_object);
+            player_update();
+        }
+    }
+    function right(){
+        if(player_x<=850){
+            player_x=player_x+block_image_width;
+            console.log("Block image width= "+block_image_width);
+            console.log("When up arrow is pressed x= "+player_x+" y= "+player_y);
+            canvas.remove(player_object);
+            player_update();
+        }
+    }
